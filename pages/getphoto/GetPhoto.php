c3f0d93c10eee5cae34ba6370c087c4a0692f1f9
Michael Greenhill <doctorjbeam@gmail.com>
2011-02-08 20:56:12 +1100

    Strengthening getphoto.php
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- pages/getphoto/GetPhoto.php
+++ pages/getphoto/GetPhoto.php
@@ -1,28 +1,51 @@
 <?php
-header('Cache-Control: max-age=604800 must-revalidate');
-header("Expires: ".gmdate('r', strtotime('+1 week'))." GMT"); // Date in the past
-$globalDir = "c:\\inetpub\\wwwroot\\www.projectnightlife.co.uk\\";
-require_once($globalDir.'backend\\global_include.php');
-
-API::Authenticate();
-
-$id = (isset($_GET['id'])) ? $_GET['id'] : die();
-$size = (isset($_GET['size'])) ? $_GET['size'] : '606x450'; // eventually change to photo gallery photo size as default
-
-$imageSvc = API::GetService("image");
-$data = $imageSvc->GetPhotoLocations($id);
-
-foreach ($data as $image)
-{
-  if ($image->size == $size)
-  {
-   // header('location: '.$image->location);
-	header('Content-type: image/jpeg');
-	print file_get_contents($image->location);
-	break;
-  }
-}
-
-// still has a bit of extra overhead for the webservice call and redirect... Much better than dynamic resizing though!
-
+	/**
+	 * GetPhoto 
+	 * 
+	 * Copyright (c) 2010-2011
+	 *
+	 * Last updated 8/02/2011
+	 *
+	 */
+	
+	// Output buffering so we don't screw up the headers
+	ob_start();
+	
+	// Set the caching headers
+	header('Cache-Control: max-age=604800 must-revalidate');
+	header("Expires: ".gmdate('r', strtotime('+1 week'))." GMT"); 
+	
+	// Global include
+	$globalDir = "c:\\inetpub\\wwwroot\\www.projectnightlife.co.uk\\";
+	require_once($globalDir.'backend\\global_include.php');
+	
+	// API Auth
+	API::Authenticate();
+	
+	// ID and sizes
+	$id		= (isset($_GET['id'])) ? $_GET['id'] : false;
+	$size	= (isset($_GET['size'])) ? $_GET['size'] : '606x450'; // eventually change to photo gallery photo size as default
+	
+	if ($id == false) { 
+		die(); 
+	}
+	
+	$imageSvc = API::GetService("image");
+	$data = $imageSvc->GetPhotoLocations($id);
+	
+	echo "<pre>"; print_r($data); echo "</pre>"; 
+	
+	foreach ($data as $image) {
+		if ($image->size == $size) {
+			// header('location: '.$image->location);
+			header('Content-type: image/jpeg');
+			print file_get_contents($image->location);
+			break;
+		}
+	}
+	
+	ob_end_flush();
+	
+	// still has a bit of extra overhead for the webservice call and redirect... Much better than dynamic resizing though!
+	
 ?>
