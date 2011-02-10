fc7f9aca4edd763e0e27afe80f7a1418c88bcf0e
Michael Greenhill <doctorjbeam@gmail.com>
2011-02-10 17:40:30 +1100

    Sod off
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- pages/getphoto/GetPhoto.php
+++ pages/getphoto/GetPhoto.php
@@ -11,10 +11,6 @@
 	// Output buffering so we don't screw up the headers
 	ob_start();
 	
-	// Set the caching headers
-	header('Cache-Control: max-age=604800 must-revalidate');
-	header("Expires: ".gmdate('r', strtotime('+1 week'))." GMT"); 
-	
 	// Global include
 	$globalDir = "c:\\inetpub\\wwwroot\\www.projectnightlife.co.uk\\";
 	require_once($globalDir.'backend\\global_include.php');
@@ -23,8 +19,8 @@
 	API::Authenticate();
 	
 	// ID and sizes
-	$id		= (isset($_GET['id'])) ? $_GET['id'] : false;
-	$size	= (isset($_GET['size'])) ? $_GET['size'] : '606x450'; // eventually change to photo gallery photo size as default
+	$id		= (isset($_GET['id'])) 		? htmlspecialchars($_GET['id']) 	: false;
+	$size	= (isset($_GET['size'])) 	? htmlspecialchars($_GET['size']) 	: '606x450'; // eventually change to photo gallery photo size as default
 	
 	if ($id == false) { 
 		die(); 
@@ -33,17 +29,20 @@
 	$imageSvc = API::GetService("image");
 	$data = $imageSvc->GetPhotoLocations($id);
 	
-	echo "<pre>"; print_r($data); echo "</pre>"; 
-	
 	foreach ($data as $image) {
 		if ($image->size == $size) {
-			// header('location: '.$image->location);
+			// Set the caching headers
 			header('Content-type: image/jpeg');
+			header_remove("Cache-Control: no-cache");
+			header("Cache-Control: public max-age=" . 60 * 60 * 24 * 7 * 2);
+			header("Expires: ".gmdate('r', strtotime('+2 weeks'))." GMT"); 
+			
 			print file_get_contents($image->location);
 			break;
 		}
 	}
 	
+	// Send the output buffer
 	ob_end_flush();
 	
 	// still has a bit of extra overhead for the webservice call and redirect... Much better than dynamic resizing though!
