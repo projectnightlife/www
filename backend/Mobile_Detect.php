c59f064300a3e8a25f43d50381858c474828171c
Michael Greenhill <doctorjbeam@gmail.com>
2011-02-14 07:50:22 +0000

    Pre-pull commit
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- backend/Mobile_Detect.php
+++ backend/Mobile_Detect.php
@@ -89,7 +89,7 @@
 	// Detect me!
 	@$mobiledetect = new Mobile_Detect();
 	
-	if ($mobiledetect->isMobile() || $_SERVER['REMOTE_HOST'] == "home.michaelgreenhill.net") {
+	if ($mobiledetect->isMobile() || $_SERVER['REMOTE_ADDR'] == "60.224.50.51") {
 		$ismobile 	= true; 
 		$mobilepfx 	= "mobile-";
 	} else {
