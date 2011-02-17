4c5c39417f0d22e97ce163c425aa1dc0c39fcb5e
Marcus Williams <marcus@projectnightlife.co.uk>
2011-02-17 22:55:12 +0000

    artist page
    Signed-off-by: Marcus Williams <marcus@projectnightlife.co.uk>

--- js/artists/artist.js
+++ js/artists/artist.js
@@ -64,5 +64,5 @@ $(document).ready(function()
 				
 				return false;
 			}).filter(":first").click();
-			$( "#tabs" ).tabs( "option", "fx", { opacity: 'toggle' } );
+			$( "#tabs" ).tabs( "option", "fx", { opacity: 'toggle', duration: 'slow' } );
 });
