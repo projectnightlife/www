9c6d834f313bfa84c959bd495f9169c1162a5616
Marcus Williams <marcus@projectnightlife.co.uk>
2011-04-04 13:29:29 +0100

    brand page
    Signed-off-by: Marcus Williams <marcus@projectnightlife.co.uk>

--- js/brands/event.js
+++ js/brands/event.js
@@ -1,3 +1,5 @@
+var selectedEvent = 3;
+
 $('#eventNext').bind('click', function()
   {
 	if($("#eventReel:animated").length == 0) {
@@ -6,6 +8,9 @@ $('#eventNext').bind('click', function()
         	left: -191 + offset
    		}, 300);
 	}
+	api.getElem("event"+selectedEvent).style.webkitTransform = 'scale(1)';
+	selectedEvent--;
+	api.getElem("event"+selectedEvent).style.webkitTransform = 'scale(1.175)';
 	return false;
   });
   $('#eventPrev').bind('click', function()
@@ -16,5 +21,8 @@ $('#eventNext').bind('click', function()
             left: 191 + offset
    	 	}, 300);
 	}
+	api.getElem("event"+selectedEvent).style.webkitTransform = 'scale(1)';
+	selectedEvent++;
+	api.getElem("event"+selectedEvent).style.webkitTransform = 'scale(1.175)';
 	return false;
   });
